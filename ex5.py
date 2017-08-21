d3ecda636ba856bbc4679ed6c39bf0f5fbd69724
babuy45 <ashabibi49@gmail.com>
2017-08-22 06:20:55 +0700

    latihan python in hardway

---
+++ ex5.py
@@ -0,0 +1,19 @@
+## Learning Python in Hard Way
+
+my_name = 'Zed A. Shaw'
+my_age = 24
+my_height = 169
+my_weight = 45
+my_eyes = 'black'
+my_teeth = 'white'
+my_hair = 'brown'
+
+print ("Let's talk about %s." % my_name)
+print ("He's %d inches tall." % my_height)
+print ("He's %d kilos heavy" % my_weight)
+print ("Actually that's not too heavy")
+print ("He's got %s eyes and %s hair" %(my_eyes, my_hair))
+print ("His teeth are usually %s depending on the coffe" % my_teeth)
+
+# this line is tricky, try to get it exactly ready.
+print ("If i add %d, %d, and %d i get %d." % (my_age, my_height, my_weight, my_age + my_height + my_weight))
